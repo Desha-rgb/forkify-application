75e345d9f76b8c380112bc6dc4cb35973a578771
Desha-rgb <mnsar7478@gmail.com>
2022-08-26 13:20:22 +0200

    updating config file

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR KEY>';
+export const KEY = 'ce4a3823-2f33-4db7-9d83-c9ab80c425aa';
 export const MODAL_CLOSE_SEC = 2.5;
